8732172603e26299ab5a737bbd38a45b361f0f80
Meredith Welch <merewelch@gmail.com>
2019-11-25 13:02:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "yaml"
 def load_library(file_name)
   lib = YAML.load_file(file_name)
   new_hash = {}
-  lib.each do |k,v|
+  lib.map do |k,v|
     new_hash[:get_meaning] = {v[1] => k}
     new_hash[:get_emoticon] = {v[0] => v[1]}
   end
