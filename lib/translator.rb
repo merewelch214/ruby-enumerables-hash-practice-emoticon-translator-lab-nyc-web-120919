e11fd2f98021793ee9d72c4a5783be775ceafccd
Meredith Welch <merewelch@gmail.com>
2019-11-25 13:03:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(file_name)
   lib.map do |k,v|
     new_hash[:get_meaning] = {v[1] => k}
     new_hash[:get_emoticon] = {v[0] => v[1]}
+    p new_hash
   end
   p new_hash
   return new_hash
